97dea2cea4488fcec9e1f08fd723890ff90380a5
SATYAM KUMAR PANDEY <defencepur360@gmail.com>
2024-05-24 15:03:16 +0530

    update app.js file

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,5 +1,7 @@
 import express from "express"
 import cors from "cors"
+import cookieParser from "cookie-parser"
+
 
 const app = express()
 
@@ -7,6 +9,12 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials:true
 }))
+
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended:true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
 
 
 // route import 
