f78e5da47226a26ba18c106f0776c4f1589b4bf0
SATYAM KUMAR PANDEY <defencepur360@gmail.com>
2024-05-24 13:10:14 +0530

    Create Auth Route

---
+++ Backend/src/routes/auth.routes.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+
+const router = Router()
+
+// register 
+
+router.route("/register")
+
+//Login
+router.route("/login")
+
+// logout 
+router.route("/logout")
+
+
+export default router;
