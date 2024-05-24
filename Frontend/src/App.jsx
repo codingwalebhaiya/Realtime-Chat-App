a731c3d0c0dba2430de5bf26bc72e696e0ead617
SATYAM KUMAR PANDEY <defencepur360@gmail.com>
2024-05-24 13:07:05 +0530

    setup the frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import DarkModeToggle from "./components/darkmode/DarkModeToggle.jsx"
+
+
+const App = () => {
+  return (
+    <div>
+    <div className=" text-black ">
+      <DarkModeToggle />
+    </div>
+    <div>
+       <h1 >Welcome to code editor </h1>
+    </div>
+     
+    </div>
+  )
+}
+
+export default App
